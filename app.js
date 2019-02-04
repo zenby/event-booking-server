063c987f482ff8cdb6f8b663012e36f22fab8021
Evgeniy Dainovich <zenbysel@gmail.com>
2019-02-04 12:29:23 +0300

    add graphql http

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-  res.send("hello");
-});
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(`
+      type RootQuery {
+        events: [String!]!
+      }
+
+      type RootMutation {
+        createEvent(name: String): String
+      }
+
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      events: () => {
+        return ["Sailing", "Coding", "Swimming"];
+      },
+      createEvent: args => args.name
+    },
+    graphiql: true
+  })
+);
 
 app.listen(3000);
